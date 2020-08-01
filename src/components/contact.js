34f5a342af0a2221668c8b05a6ba1e5b777a0a9e
frankopkusianwar <okirorfrank3@gmail.com>
2020-08-01 16:19:10 +0300

    Add content to contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,49 @@
 const contactPage = () => {
   document.querySelector('.main').innerHTML = '';
   const main = document.querySelector('.main');
-  const p = document.createElement('p');
-  p.textContent = 'contact';
-  main.appendChild(p);
+  
+  const contactDiv = document.createElement('div')
+  contactDiv.setAttribute('class', 'row')
+
+  const contactDiv1 = document.createElement('div')
+  contactDiv1.setAttribute('class', 'col-5 bg-white text-dark p-3')
+  contactDiv1.innerHTML = `<h3 class="mb-3">Open Time</h3>
+  <div class="row">
+    <div class="col-6">
+      <p><b>MON :</b> 08:00 AM - 05:00 PM</P>
+      <p><b>TUE :</b> 08:00 AM - 05:00 PM</P>
+      <p><b>WED :</b> 08:00 AM - 05:00 PM</P>
+    </div>
+    <div class="col-6">
+      <p><b>THUR :</b> 08:00 AM - 05:00 PM</P>
+      <p><b>FRI :</b> 08:00 AM - 05:00 PM</P>
+      <p><b>SAT :</b> 08:00 AM - 05:00 PM</P>
+      <p><b>SUN :</b> CLOSED</P>
+    </div>
+    <div>
+  `
+
+  const btn = document.createElement('div')
+  btn.setAttribute('class', 'col-2')
+
+
+  const contactDiv2 = document.createElement('div')
+  contactDiv2.setAttribute('class', 'col-5 bg-white text-dark')
+  contactDiv2.innerHTML = `<h3>Contact Us</h3>
+  <p>MON:</P>
+  `
+
+
+
+  contactDiv.appendChild(contactDiv1)
+  contactDiv.appendChild(btn)
+  contactDiv.appendChild(contactDiv2)
+
+
+  main.appendChild(contactDiv)
+
+
+
 };
 
 export default contactPage;
